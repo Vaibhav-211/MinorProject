96ad7edaf59012f6d61167c11c02621b8bcd7c22
Srashti Rai <141318435+srashti-19@users.noreply.github.com>
2024-12-08 17:58:46 +0530

    Add files via upload

--- routes/event.js
+++ routes/event.js
@@ -1,88 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { eventSchema} = require("../schema.js");
 const Event = require("../models/event.js");
+const { isLoggedIn, isOwner, validateEvent } = require("../middleware.js");
+const eventController = require("../controllers/events.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
-
-const validateEvent = (req, res, next) => {
-  // let { error } = eventSchema.validate(req.body);
-  let { error } = eventSchema.validate({ event: req.body.event })
-  if (error) {
-    let errMsg = error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
-// Index Route
-router.get(
-  "/",
-  wrapAsync(async (req, res) => {
-    const allEvents = await Event.find({});
-    res.render("events/index.ejs", { allEvents });
-  })
-);
+router
+  .route("/")
+  .get(wrapAsync(eventController.index))
+  .post(
+    isLoggedIn,
+    upload.single("event[image]"),
+    validateEvent,
+    wrapAsync(eventController.createEvent)
+  );
 
 // New Route
-router.get("/new", (req, res) => {
-  res.render("events/new.ejs");
-});
+router.get("/new", isLoggedIn, eventController.renderNewForm);
 
-// Show Route
-router.get(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const event = await Event.findById(id).populate("reviews");
-    res.render("events/show.ejs", { event });
-  })
-);
-
-// Create Route
-router.post(
-  "/",
-  validateEvent,
-  wrapAsync(async (req, res) => {
-    const newEvent = new Event(req.body.event);
-    await newEvent.save();
-    req.flash("success","New Event Created!");
-    res.redirect("/events");
-  })
-);
+router
+  .route("/:id")
+  .get(wrapAsync(eventController.showEvent))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("event[image]"),
+    validateEvent,
+    wrapAsync(eventController.updateEvent)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(eventController.destroyEvent));
 
 // Edit Route
 router.get(
   "/:id/edit",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const event = await Event.findById(id);
-    res.render("events/edit.ejs", { event });
-  })
-);
-
-// Update Route
-router.put(
-  "/:id",
-  validateEvent,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Event.findByIdAndUpdate(id, { ...req.body.event });
-    res.redirect(`/events/${id}`);
-  })
-);
-
-// Delete Route
-router.delete(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedEvent = await Event.findByIdAndDelete(id);
-    console.log(deletedEvent);
-    res.redirect("/events");
-  })
+  isLoggedIn,
+  isOwner,
+  wrapAsync(eventController.renderEditForm)
 );
 
 module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -1,50 +1,30 @@
 const express = require("express");
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Event = require("../models/event.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+const reviewController = require("../controllers/reviews.js");
 
-// Reviews 
-// Post Review Route
+// Reviews // Post Review Route
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
-  wrapAsync(async (req, res) => {
-    let event = await Event.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    event.reviews.push(newReview);
-
-    await newReview.save();
-    await event.save();
-
-    res.redirect(`/events/${event._id}`);
-
-  })
+  wrapAsync(reviewController.createReview)
 );
-
 
 // Delete  Review Route
 router.delete(
   "/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Event.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/events/${id}`);
-  })
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(reviewController.destroyReview)
 );
 
 module.exports = router;
